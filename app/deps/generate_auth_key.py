bece70b041c48cac8a814d2ce204b369de9bb176
NaturaAdnyana <natura538@gmail.com>
2023-06-03 23:42:38 +0800

    recode generate_auth_key.py

--- app/deps/generate_auth_key.py
+++ app/deps/generate_auth_key.py
@@ -1,3 +1,5 @@
-from encrypt import generate_key
+from cryptography.fernet import Fernet
 
-generate_key()
+key = Fernet.generate_key()
+with open("secret.key", "wb") as key_file:
+    key_file.write(key)
