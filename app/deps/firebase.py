289a21168cbb800ff50fec88e8ebf0ba81b6009c
e-edsen <edy.setiawan02@gmail.com>
2023-06-10 13:42:43 +0700

    oneliner env

--- app/deps/firebase.py
+++ app/deps/firebase.py
@@ -2,23 +2,26 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
-cred = credentials.Certificate({
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
-    "universal_domain": os.getenv("FIREBASE_UNIVERSAL_DOMAIN")
-})
+# cred = credentials.Certificate({
+#     "type": os.getenv("FIREBASE_TYPE"),
+#     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+#     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+#     "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+#     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+#     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+#     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+#     "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+#     "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+#     "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+#     "universal_domain": os.getenv("FIREBASE_UNIVERSAL_DOMAIN")
+# })
+
+data = json.loads(os.getenv("FIREBASE"))
+cred = credentials.Certificate(data)
 
 firebase_admin.initialize_app(cred, {'storageBucket': 'hidup-sehat-server.appspot.com'})
-
 db = firestore.client()
