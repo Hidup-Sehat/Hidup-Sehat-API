7455106c42bfed6b0f8f41abf8ea4203af413683
e-edsen <edy.setiawan02@gmail.com>
2023-06-16 16:21:17 +0700

    feeds from last user emotion if exist, data adding from before

--- app/schemas/feed.py
+++ app/schemas/feed.py
@@ -5,7 +5,8 @@ from datetime import date
 from typing import List
 
 class GetFeeds(BaseModel):
-  user_current_mood: str = Field(..., example="hari ini aku {emosi postif, emosi negatif} yang berasal dari {asal emosi}, {cerita}")
+  # user_current_mood: str = Field(..., example="hari ini aku {emosi postif, emosi negatif} yang berasal dari {asal emosi}, {cerita}")
+  user_uid: str = Field(..., example="GvmIHoklZROQRiRIfN3dc3hCLAg1")
 
 class GetFeedUrl(BaseModel):
   title: str
